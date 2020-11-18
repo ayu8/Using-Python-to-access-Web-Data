2ded81125fca736f8668a3ab9a02bed5fc02cbd9
Ayush Singh <59230661+ayu8@users.noreply.github.com>
2020-11-19 04:57:06 +0530

    Update Extracting_data_from_XML.py

--- Assignment 4/Extracting_data_from_XML.py
+++ Assignment 4/Extracting_data_from_XML.py
@@ -1,1 +1,26 @@
-.
+import xml.etree.ElementTree as ET
+import urllib.request, urllib.parse, urllib.error
+import ssl
+
+url = input("Enter location: ")
+print("Retrieving", url)
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+data = urllib.request.urlopen(url).read()
+print("Retrieved", len(data), "characters")
+
+tree = ET.fromstring(data)
+lst = tree.findall("comments/comment")
+
+cmnts = 0
+total = 0
+
+for i in lst:
+	total += int(i.find("count").text)
+	cmnts += 1
+print("Count:", cmnts)
+print("Sum:", total)
