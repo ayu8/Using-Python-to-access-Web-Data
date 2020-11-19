1ee7bacc8c44c93932cdb50b30940bc44f7ab02c
Ayush Singh <59230661+ayu8@users.noreply.github.com>
2020-11-19 12:05:46 +0530

    Update Extracting_data_from_JSON.py

--- Assignment 5/Extracting_data_from_JSON.py
+++ Assignment 5/Extracting_data_from_JSON.py
@@ -1,1 +1,24 @@
-.
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import json
+
+url = input("Enter location: ")
+print("Retrieving", url)
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+data = urllib.request.urlopen(url).read()
+print("Retrieved", len(data), "characters")
+
+js = json.loads(data)
+cmnts = total = 0
+
+for i in js["comments"]:
+	total += i["count"]
+	cmnts += 1
+
+print("Count:", cmnts)
+print("Sum:", total)
